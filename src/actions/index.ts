a0feffcda6826a36523a5b9700f31ce3e6b7799b
James Tognola <jamesptognola@gmail.com>
2022-11-23 00:13:53 +0000

    Removing code gen services, keep store and slices in one place for now.

--- src/actions/index.ts
+++
@@ -1,8 +0,0 @@
-export const SEARCH_RESULTS = 'SEARCH_RESULTS';
-
-export function searchResults(results: any) {
-  return {
-    type: SEARCH_RESULTS,
-    results,
-  };
-}
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,18 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { setupListeners } from '@reduxjs/toolkit/query';
-import { filmSplitApi } from './filmApi';
 import { filmsSlice } from './filmSlice';
 
 export const store = configureStore({
   reducer: {
-    [filmSplitApi.reducerPath]: filmSplitApi.reducer,
     [filmsSlice.name]: filmsSlice.reducer,
   },
-
-  // Adding the api middleware enables caching, invalidation, polling,
-  // and other useful features of `rtk-query`.
-  middleware: getDefaultMiddleware =>
-    getDefaultMiddleware().concat(filmSplitApi.middleware),
 });
 
 // optional, but required for refetchOnFocus/refetchOnReconnect behaviors
